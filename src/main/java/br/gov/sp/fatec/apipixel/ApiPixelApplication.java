a4ed0978923b44ebe5adfb13b4a08fb68e8024d6
wdeus <wagner.silva96@fatec.sp.gov.br>
2024-03-29 18:01:54 -0300

    :tada: configuração inicial do projeto

---
+++ src/main/java/br/gov/sp/fatec/apipixel/ApiPixelApplication.java
@@ -0,0 +1,13 @@
+package br.gov.sp.fatec.apipixel;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiPixelApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiPixelApplication.class, args);
+	}
+
+}
