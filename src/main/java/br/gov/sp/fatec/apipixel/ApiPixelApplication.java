9e14a9acf371d87fc1ee30cd12204774627a2372
41issonm <164199364+41issonm@users.noreply.github.com>
2024-04-07 17:15:48 -0300

    feature-86azvat8w:adiciao de cors

--- src/main/java/br/gov/sp/fatec/apipixel/ApiPixelApplication.java
+++ src/main/java/br/gov/sp/fatec/apipixel/ApiPixelApplication.java
@@ -2,8 +2,10 @@ package br.gov.sp.fatec.apipixel;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @SpringBootApplication
+@CrossOrigin("*")
 public class ApiPixelApplication {
 
 	public static void main(String[] args) {
