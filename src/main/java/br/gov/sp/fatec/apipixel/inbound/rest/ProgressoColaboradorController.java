8ff04352495efe8ee63b1eb327f957f5a0bc7469
wdeus <wagner.silva96@fatec.sp.gov.br>
2024-04-08 20:40:49 -0300

    feat-86azvavzx - Adiciona configuração CORS

--- src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/ProgressoColaboradorController.java
+++ src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/ProgressoColaboradorController.java
@@ -5,16 +5,14 @@ import br.gov.sp.fatec.apipixel.core.domain.projection.TrilhaColaboradorProjecti
 import br.gov.sp.fatec.apipixel.core.usecase.progresso.CarregarTrilhaPorColaboradorUC;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("api/progresso-colaborador")
 @RequiredArgsConstructor
+@CrossOrigin("*")
 public class ProgressoColaboradorController {
 
     private final CarregarTrilhaPorColaboradorUC carregarTrilhaPorColaboradorUC;
