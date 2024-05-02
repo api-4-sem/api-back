60216782ed1d3ae691d7c5685f649b344c402e06
41issonm <164199364+41issonm@users.noreply.github.com>
2024-05-01 21:54:31 -0300

    fix:ajuste no endpoint da empresa e
    Ajuste nas rotas e adicao de cabecalho cors

--- src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/EmpresaController.java
+++ src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/EmpresaController.java
@@ -3,38 +3,32 @@ package br.gov.sp.fatec.apipixel.inbound.rest;
 import br.gov.sp.fatec.apipixel.core.domain.entity.Empresa;
 import br.gov.sp.fatec.apipixel.core.domain.projection.EmpresaProjection;
 import br.gov.sp.fatec.apipixel.core.usecase.empresa.CarregarEmpresaUC;
+import br.gov.sp.fatec.apipixel.core.usecase.empresa.CriarEmpresaUC; // Importar a classe correta
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import br.gov.sp.fatec.apipixel.core.domain.projection.EmpresaProjection;
-import br.gov.sp.fatec.apipixel.core.usecase.empresa.CarregarEmpresaUC;
-import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
 @RestController
-@RequestMapping("api/carregar-empresas")
+@CrossOrigin("*")
+@RequestMapping("api")
 @RequiredArgsConstructor
 public class EmpresaController {
 
     private final CarregarEmpresaUC carregarEmpresaUC;
-    private final CarregarEmpresaUC criarEmpresaUC;
+    private final CriarEmpresaUC criarEmpresaUC; 
 
-
-    @GetMapping
-    public ResponseEntity<List<EmpresaProjection>> findAll(){
+    @GetMapping("/carregar-empresas")
+    public ResponseEntity<List<EmpresaProjection>> findAll() {
         return ResponseEntity.ok(carregarEmpresaUC.executar());
     }
 
-    @PostMapping
-    public ResponseEntity<Empresa> criarEmpresa() {
-        Empresa empresaCriada = (Empresa) criarEmpresaUC.executar();
+  
+    @PostMapping("/criar-empresas")
+    public ResponseEntity<Empresa> criarEmpresa(@RequestBody Empresa novaEmpresa) {
+        Empresa empresaCriada = criarEmpresaUC.executar(novaEmpresa);
         return ResponseEntity.status(HttpStatus.CREATED).body(empresaCriada);
     }
-
 }
