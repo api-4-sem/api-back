2c681a28c66ca2956e432b009edfa09a211c2ecf
Jhonny Freitas Dutra <dutrajy@gmail.com>
2024-05-03 12:52:30 -0300

    feat: Criar avaliacao

--- src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/AvaliacaoController.java
+++ src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/AvaliacaoController.java
@@ -1,8 +1,12 @@
 package br.gov.sp.fatec.apipixel.inbound.rest;
 
+import br.gov.sp.fatec.apipixel.core.domain.entity.Avaliacao;
 import br.gov.sp.fatec.apipixel.core.domain.projection.AvaliacaoProjection;
 import br.gov.sp.fatec.apipixel.core.usecase.avaliacao.CarregarAvaliacaoUC;
+import br.gov.sp.fatec.apipixel.core.usecase.avaliacao.CriarAvaliacaoUC;
 import lombok.RequiredArgsConstructor;
+
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
@@ -15,9 +19,16 @@ import java.util.List;
 public class AvaliacaoController {
 
     private final CarregarAvaliacaoUC carregarAvaliacaoEmpresaUC;
+    private final CriarAvaliacaoUC criarAvaliacaoUC;
 
     @GetMapping("{empresaId}")
     public ResponseEntity<List<AvaliacaoProjection>> carregarColaborador(@PathVariable("empresaId") Long empresaId){
         return ResponseEntity.ok(carregarAvaliacaoEmpresaUC.executar(empresaId));
+    }
+
+    @PostMapping("/criar-avaliacao")
+    public ResponseEntity<Avaliacao> criarAvaliacao(@RequestBody Avaliacao avaliacao) {
+        Avaliacao avaliacaoCriada = criarAvaliacaoUC.executar(avaliacao);
+        return ResponseEntity.status(HttpStatus.CREATED).body(avaliacaoCriada);
     }
 }
