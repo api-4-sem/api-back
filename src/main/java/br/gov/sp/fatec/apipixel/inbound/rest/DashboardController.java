b6daa976814e0293df675c3fb967155fe3edf785
wdeus <wagner.silva96@fatec.sp.gov.br>
2024-05-24 18:50:04 -0300

    feat - Adiciona API para carregar trilhas da empresa

--- src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/DashboardController.java
+++ src/main/java/br/gov/sp/fatec/apipixel/inbound/rest/DashboardController.java
@@ -1,8 +1,11 @@
 package br.gov.sp.fatec.apipixel.inbound.rest;
 
 import br.gov.sp.fatec.apipixel.core.domain.command.CarregarDadosExpertiseCommand;
+import br.gov.sp.fatec.apipixel.core.domain.command.CarregarTrilhaPorEmpresaCommand;
 import br.gov.sp.fatec.apipixel.core.domain.dto.DadosExpertisesDto;
+import br.gov.sp.fatec.apipixel.core.domain.dto.TrilhaDto;
 import br.gov.sp.fatec.apipixel.core.usecase.dashboard.CarregarDadosExpertiseUC;
+import br.gov.sp.fatec.apipixel.core.usecase.trilha.CarregarTrilhaPorEmpresa;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -16,6 +19,7 @@ import java.util.List;
 public class DashboardController {
 
     private final CarregarDadosExpertiseUC carregarDadosExpertiseUC;
+    private final CarregarTrilhaPorEmpresa carregarTrilhaPorEmpresa;
 
     @GetMapping("expertises/empresa/{empresaId}/trilha/{trilhaId}")
     public ResponseEntity<List<DadosExpertisesDto>> carregarColaborador(@PathVariable("empresaId") Long empresaId,
@@ -23,4 +27,11 @@ public class DashboardController {
         CarregarDadosExpertiseCommand command = new CarregarDadosExpertiseCommand(trilhaId,empresaId);
         return ResponseEntity.ok(carregarDadosExpertiseUC.executar(command));
     }
+
+    @GetMapping("trilhas/empresa/{empresaId}")
+    public ResponseEntity<List<TrilhaDto>> carregarTrilha(@PathVariable("empresaId") Long empresaId){
+        CarregarTrilhaPorEmpresaCommand command = new CarregarTrilhaPorEmpresaCommand(empresaId);
+        return ResponseEntity.ok(carregarTrilhaPorEmpresa.executar(command));
+    }
+
 }
